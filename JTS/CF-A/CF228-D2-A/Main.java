51979440dc5a933136b6a4dbc29b83643a641add
Yash Kumar <Yash.Kumar@niceactimize.com>
2019-03-19 00:35:02 +0530

    Code 112

---
+++ JTS/CF-A/CF228-D2-A/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+//    Date : 2019-03-19
+//  Author : Yash Kumar
+// Problem : http://codeforces.com/contest/228/problem/A
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] ary = new int[4];
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            ary[i] = sc.nextInt();
+        }
+        Arrays.sort(ary);
+        for (int i = 1; i <= 3; i++) {
+            if (ary[i - 1] == ary[i]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
