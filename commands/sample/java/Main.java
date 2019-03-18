b2b83f1dbafed1ce9c800307c562e604d1b5e129
Yash Kumar <Yash.Kumar@niceactimize.com>
2019-03-18 22:34:33 +0530

    java init remove cpp

---
+++ commands/sample/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//    Date : <<<date_now>>>
+//  Author : Yash Kumar
+// Problem : <<<src_url>>>
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n);
+    }
+}
